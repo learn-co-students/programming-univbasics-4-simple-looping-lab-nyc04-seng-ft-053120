ef338f2e0c770b130d932809f790c50680482833
Alvee Noor Akand <alvee.akand@gmail.com>
2020-06-02 12:19:10 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,20 @@
 # Write your methods here
+def loop_message_five_times(str)
+  5.times do
+    puts str
+  end
+end
+
+def loop_message_n_times(str, n)
+  n.times do
+    puts str
+  end
+end
+
+def output_array(arr)
+  arr.each {|el| puts el}
+end
+
+def return_string_array(arr)
+  arr.each_index {|idx| arr[idx] = arr[idx].to_s}
+end
