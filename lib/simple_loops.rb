e166bc79fd7d6028918802de9a243c3ffc2d504b
Rouan Thompson <rouan.thompson@gmail.com>
2020-06-08 20:48:28 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,21 @@
-# Write your methods here
+def loop_message_five_times(message)
+  5.times {puts message}
+end
+
+def loop_message_n_times(message, n_output)
+  n_output.times {puts message}
+end
+
+def output_array(message)
+  message.each {|element| puts element}
+end
+
+def return_string_array(array)
+  converted = []
+  i = 0
+  array.each {|element|
+    converted[i] = element.to_s
+    i += 1
+  }
+  converted
+end
