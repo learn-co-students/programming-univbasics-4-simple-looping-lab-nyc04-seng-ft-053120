de2c2998ec32622c817795ef9174f7900c359c9e
Francisco Andaur <francisco.andaur@icloud.com>
2020-06-08 16:18:49 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,29 @@
-# Write your methods here
+def loop_message_five_times(message)
+  i = 0
+  while i < 5 do
+    puts message
+      i +=1
+  end
+end
+
+def loop_message_n_times(message, num)
+  i = 0
+  while i <= num do
+    puts message
+    i += 1
+  end
+end
+
+def output_array(array)
+  puts array
+end
+
+def return_string_array(array)
+  i = 0
+  new_array = []
+  while i < array.length do
+    new_array.push(array[i].to_s)
+    i += 1
+  end
+  new_array
+end
